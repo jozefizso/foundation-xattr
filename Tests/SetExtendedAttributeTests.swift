9e08729b3ba585f0ab0b22a4dc980dd9f34d463b
Jozef Izso <jizso@slido.com>
2023-03-19 13:54:18 +0100

    Skip the `test_URL_setExtendedAttribute` test

--- Tests/SetExtendedAttributeTests.swift
+++ Tests/SetExtendedAttributeTests.swift
@@ -3,6 +3,8 @@ import XCTest
 
 final class SetExtendedAttributeTests: XCTestCase {
     func test_URL_setExtendedAttribute() throws {
+        throw XCTSkip("TODO: Rewrite this integration test")
+
         // Arrange
         let file = URL(fileURLWithPath: "/Users/jozef/Developer/foundation-xattr/tmp/myfile.txt")
         let attributeValue = "value".data(using: .utf8)!
